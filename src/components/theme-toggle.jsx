523072f2a825ee0a91eb3a388677c073b76451ab
c99-dev <c99@kakao.com>
2025-05-26 15:14:14 +0900

    feat: 테마 토글 컴포넌트 추가

---
+++ src/components/theme-toggle.jsx
@@ -0,0 +1,49 @@
+'use client';
+
+import * as React from 'react';
+import { Moon, Sun, Monitor } from 'lucide-react';
+import { Button } from './ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { useTheme } from '../contexts/ThemeContext';
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  // 테마 변경 및 드롭다운 닫기 함수
+  const handleThemeChange = (newTheme) => {
+    setTheme(newTheme);
+    setOpen(false); // 드롭다운 닫기
+  };
+
+  return (
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">테마 변경</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => handleThemeChange('light')}>
+          <Sun className="mr-2 h-4 w-4" />
+          <span>라이트</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleThemeChange('dark')}>
+          <Moon className="mr-2 h-4 w-4" />
+          <span>다크</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleThemeChange('system')}>
+          <Monitor className="mr-2 h-4 w-4" />
+          <span>시스템</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
