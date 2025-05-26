67d46c70335c692e5589f49d0863fe7513cd5574
c99-dev <c99@kakao.com>
2025-05-26 15:31:06 +0900

    refactor: 드롭다운 상태 관리 로직 간소화 및 onClick을 onSelect로 변경

--- src/components/theme-toggle.jsx
+++ src/components/theme-toggle.jsx
@@ -1,6 +1,3 @@
-'use client';
-
-import * as React from 'react';
 import { Moon, Sun, Monitor } from 'lucide-react';
 import { Button } from './ui/button';
 import {
@@ -13,16 +10,9 @@ import { useTheme } from '../contexts/ThemeContext';
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  // 테마 변경 및 드롭다운 닫기 함수
-  const handleThemeChange = (newTheme) => {
-    setTheme(newTheme);
-    setOpen(false); // 드롭다운 닫기
-  };
 
   return (
-    <DropdownMenu open={open} onOpenChange={setOpen}>
+    <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
@@ -31,15 +21,15 @@ export function ThemeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => handleThemeChange('light')}>
+        <DropdownMenuItem onSelect={() => setTheme('light')}>
           <Sun className="mr-2 h-4 w-4" />
           <span>라이트</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleThemeChange('dark')}>
+        <DropdownMenuItem onSelect={() => setTheme('dark')}>
           <Moon className="mr-2 h-4 w-4" />
           <span>다크</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleThemeChange('system')}>
+        <DropdownMenuItem onSelect={() => setTheme('system')}>
           <Monitor className="mr-2 h-4 w-4" />
           <span>시스템</span>
         </DropdownMenuItem>
